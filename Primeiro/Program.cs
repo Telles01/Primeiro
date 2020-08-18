3b9415a056fad5ffdda37077f481f4bee24d3f34
Bernard Telles <bernardtelgon@gmail.com>
2020-08-18 02:16:59 -0300

    Add project files.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning!");
+        }
+    }
+}
+
